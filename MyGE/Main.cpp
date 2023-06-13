5d1f474cb99836e167b5d103463fe3f00fa35c7b
DESKTOP-R0JRRCE\Leo <hypixel.block@gmail.com>
2023-06-14 03:32:18 +0900

    実行exeからあーだこーだしたポリゴンが表示されるところまでを確認できる まだコメントアウトはない

--- MyGE/Main.cpp
+++ MyGE/Main.cpp
@@ -1,44 +1,96 @@
-#include <vulkan/vulkan.hpp>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
 
-//https://chaosplant.tech/do/vulkan/
-//参考
+const uint32_t screenWidth = 640;
+const uint32_t screenHeight = 480;
+
 int main() {
-    //Vulkanインスタンスを作成
+    if (!glfwInit())
+        return -1;
+
+    uint32_t requiredExtensionsCount;
+    const char** requiredExtensions = glfwGetRequiredInstanceExtensions(&requiredExtensionsCount);
+
     vk::InstanceCreateInfo createInfo;
-    vk::UniqueInstance instance = vk::createInstanceUnique(createInfo);
-    //Vulkanインスタンスから使用できる物理GPUデバイスを列挙
+    createInfo.enabledExtensionCount = requiredExtensionsCount;
+    createInfo.ppEnabledExtensionNames = requiredExtensions;
+
+    vk::UniqueInstance instance;
+    instance = vk::createInstanceUnique(createInfo);
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window;
+    window = glfwCreateWindow(screenWidth, screenHeight, "GLFW Test Window", NULL, NULL);
+    if (!window) {
+        const char* err;
+        glfwGetError(&err);
+        std::cout << err << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    VkSurfaceKHR c_surface;
+    auto result = glfwCreateWindowSurface(instance.get(), window, nullptr, &c_surface);
+    if (result != VK_SUCCESS) {
+        const char* err;
+        glfwGetError(&err);
+        std::cout << err << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    vk::UniqueSurfaceKHR surface{ c_surface, instance.get() };
+
     std::vector<vk::PhysicalDevice> physicalDevices = instance->enumeratePhysicalDevices();
-    
-    //デバイス選択に関わる宣言
+
     vk::PhysicalDevice physicalDevice;
     bool existsSuitablePhysicalDevice = false;
     uint32_t graphicsQueueFamilyIndex;
-    //デバイス列挙から使用できるデバイスを探して設定
+
     for (size_t i = 0; i < physicalDevices.size(); i++) {
         std::vector<vk::QueueFamilyProperties> queueProps = physicalDevices[i].getQueueFamilyProperties();
         bool existsGraphicsQueue = false;
 
         for (size_t j = 0; j < queueProps.size(); j++) {
-            if (queueProps[j].queueFlags & vk::QueueFlagBits::eGraphics) {
+            if (queueProps[j].queueFlags & vk::QueueFlagBits::eGraphics &&
+                physicalDevices[i].getSurfaceSupportKHR(j, surface.get())) {
                 existsGraphicsQueue = true;
-                graphicsQueueFamilyIndex = static_cast<uint32_t>(j);
+                graphicsQueueFamilyIndex = j;
                 break;
             }
         }
 
-        if (existsGraphicsQueue) {
+        std::vector<vk::ExtensionProperties> extProps = physicalDevices[i].enumerateDeviceExtensionProperties();
+        bool supportsSwapchainExtension = false;
+
+        for (size_t j = 0; j < extProps.size(); j++) {
+            if (std::string_view(extProps[j].extensionName.data()) == VK_KHR_SWAPCHAIN_EXTENSION_NAME) {
+                supportsSwapchainExtension = true;
+                break;
+            }
+        }
+
+        if (existsGraphicsQueue && supportsSwapchainExtension) {
             physicalDevice = physicalDevices[i];
             existsSuitablePhysicalDevice = true;
             break;
         }
     }
+
     if (!existsSuitablePhysicalDevice) {
         std::cerr << "使用可能な物理デバイスがありません。" << std::endl;
         return -1;
     }
-    //デバイスのキューに割り当てる情報を設定
+
     vk::DeviceCreateInfo devCreateInfo;
+
+    auto devRequiredExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+    devCreateInfo.enabledExtensionCount = devRequiredExtensions.size();
+    devCreateInfo.ppEnabledExtensionNames = devRequiredExtensions.begin();
+
     vk::DeviceQueueCreateInfo queueCreateInfo[1];
     queueCreateInfo[0].queueFamilyIndex = graphicsQueueFamilyIndex;
     queueCreateInfo[0].queueCount = 1;
@@ -49,69 +101,44 @@ int main() {
 
     devCreateInfo.pQueueCreateInfos = queueCreateInfo;
     devCreateInfo.queueCreateInfoCount = 1;
+
     vk::UniqueDevice device = physicalDevice.createDeviceUnique(devCreateInfo);
 
     vk::Queue graphicsQueue = device->getQueue(graphicsQueueFamilyIndex, 0);
 
-    vk::CommandPoolCreateInfo cmdPoolCreateInfo;
-    cmdPoolCreateInfo.queueFamilyIndex = graphicsQueueFamilyIndex;
-    vk::UniqueCommandPool cmdPool = device->createCommandPoolUnique(cmdPoolCreateInfo);
+    vk::SurfaceCapabilitiesKHR surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR(surface.get());
+    std::vector<vk::SurfaceFormatKHR> surfaceFormats = physicalDevice.getSurfaceFormatsKHR(surface.get());
+    std::vector<vk::PresentModeKHR> surfacePresentModes = physicalDevice.getSurfacePresentModesKHR(surface.get());
 
-    vk::CommandBufferAllocateInfo cmdBufAllocInfo;
-    cmdBufAllocInfo.commandPool = cmdPool.get();
-    cmdBufAllocInfo.commandBufferCount = 1;
-    cmdBufAllocInfo.level = vk::CommandBufferLevel::ePrimary;
-    std::vector<vk::UniqueCommandBuffer> cmdBufs =
-        device->allocateCommandBuffersUnique(cmdBufAllocInfo);
+    vk::SurfaceFormatKHR swapchainFormat = surfaceFormats[0];
+    vk::PresentModeKHR swapchainPresentMode = surfacePresentModes[0];
 
-    vk::ImageCreateInfo imgCreateInfo;
-    imgCreateInfo.imageType = vk::ImageType::e2D;
-    imgCreateInfo.extent = vk::Extent3D(640, 480, 1);
-    imgCreateInfo.mipLevels = 1;
-    imgCreateInfo.arrayLayers = 1;
-    imgCreateInfo.format = vk::Format::eR8G8B8A8Unorm;
-    imgCreateInfo.tiling = vk::ImageTiling::eLinear;
-    imgCreateInfo.initialLayout = vk::ImageLayout::eColorAttachmentOptimal;
-    imgCreateInfo.usage = vk::ImageUsageFlagBits::eColorAttachment;
-    imgCreateInfo.sharingMode = vk::SharingMode::eExclusive;
-    imgCreateInfo.samples = vk::SampleCountFlagBits::e1;
+    vk::SwapchainCreateInfoKHR swapchainCreateInfo;
+    swapchainCreateInfo.surface = surface.get();
+    swapchainCreateInfo.minImageCount = surfaceCapabilities.minImageCount + 1;
+    swapchainCreateInfo.imageFormat = swapchainFormat.format;
+    swapchainCreateInfo.imageColorSpace = swapchainFormat.colorSpace;
+    swapchainCreateInfo.imageExtent = surfaceCapabilities.currentExtent;
+    swapchainCreateInfo.imageArrayLayers = 1;
+    swapchainCreateInfo.imageUsage = vk::ImageUsageFlagBits::eColorAttachment;
+    swapchainCreateInfo.imageSharingMode = vk::SharingMode::eExclusive;
+    swapchainCreateInfo.preTransform = surfaceCapabilities.currentTransform;
+    swapchainCreateInfo.presentMode = swapchainPresentMode;
+    swapchainCreateInfo.clipped = VK_TRUE;
 
-    vk::UniqueImage image = device->createImageUnique(imgCreateInfo);
+    vk::UniqueSwapchainKHR swapchain = device->createSwapchainKHRUnique(swapchainCreateInfo);
 
-    vk::PhysicalDeviceMemoryProperties memProps = physicalDevice.getMemoryProperties();
-
-    vk::MemoryRequirements imgMemReq = device->getImageMemoryRequirements(image.get());
-
-    vk::MemoryAllocateInfo imgMemAllocInfo;
-    imgMemAllocInfo.allocationSize = imgMemReq.size;
-
-    bool suitableMemoryTypeFound = false;
-    for (size_t i = 0; i < memProps.memoryTypeCount; i++) {
-        if (imgMemReq.memoryTypeBits & (1 << i)) {
-            imgMemAllocInfo.memoryTypeIndex = static_cast<uint32_t>(i);
-            suitableMemoryTypeFound = true;
-            break;
-        }
-    }
-
-    if (!suitableMemoryTypeFound) {
-        std::cerr << "使用可能なメモリタイプがありません。" << std::endl;
-        return -1;
-    }
-
-    vk::UniqueDeviceMemory imgMem = device->allocateMemoryUnique(imgMemAllocInfo);
-
-    device->bindImageMemory(image.get(), imgMem.get(), 0);
+    std::vector<vk::Image> swapchainImages = device->getSwapchainImagesKHR(swapchain.get());
 
     vk::AttachmentDescription attachments[1];
-    attachments[0].format = vk::Format::eR8G8B8A8Unorm;
+    attachments[0].format = swapchainFormat.format;
     attachments[0].samples = vk::SampleCountFlagBits::e1;
-    attachments[0].loadOp = vk::AttachmentLoadOp::eDontCare;
+    attachments[0].loadOp = vk::AttachmentLoadOp::eClear;
     attachments[0].storeOp = vk::AttachmentStoreOp::eStore;
     attachments[0].stencilLoadOp = vk::AttachmentLoadOp::eDontCare;
     attachments[0].stencilStoreOp = vk::AttachmentStoreOp::eDontCare;
     attachments[0].initialLayout = vk::ImageLayout::eUndefined;
-    attachments[0].finalLayout = vk::ImageLayout::eGeneral;
+    attachments[0].finalLayout = vk::ImageLayout::ePresentSrcKHR;
 
     vk::AttachmentReference subpass0_attachmentRefs[1];
     subpass0_attachmentRefs[0].attachment = 0;
@@ -137,12 +164,12 @@ int main() {
     viewports[0].y = 0.0;
     viewports[0].minDepth = 0.0;
     viewports[0].maxDepth = 1.0;
-    viewports[0].width = 640;
-    viewports[0].height = 480;
+    viewports[0].width = screenWidth;
+    viewports[0].height = screenHeight;
 
     vk::Rect2D scissors[1];
-    scissors[0].offset = vk::Offset2D { 0, 0 };
-    scissors[0].extent = vk::Extent2D { 640, 480};
+    scissors[0].offset = vk::Offset2D{ 0, 0 };
+    scissors[0].extent = vk::Extent2D{ screenWidth, screenHeight };
 
     vk::PipelineViewportStateCreateInfo viewportState;
     viewportState.viewportCount = 1;
@@ -192,6 +219,40 @@ int main() {
 
     vk::UniquePipelineLayout pipelineLayout = device->createPipelineLayoutUnique(layoutCreateInfo);
 
+    size_t vertSpvFileSz = std::filesystem::file_size("./shader/shader.vert.spv");
+
+    std::ifstream vertSpvFile("./shader/shader.vert.spv", std::ios_base::binary);
+
+    std::vector<char> vertSpvFileData(vertSpvFileSz);
+    vertSpvFile.read(vertSpvFileData.data(), vertSpvFileSz);
+
+    vk::ShaderModuleCreateInfo vertShaderCreateInfo;
+    vertShaderCreateInfo.codeSize = vertSpvFileSz;
+    vertShaderCreateInfo.pCode = reinterpret_cast<const uint32_t*>(vertSpvFileData.data());
+
+    vk::UniqueShaderModule vertShader = device->createShaderModuleUnique(vertShaderCreateInfo);
+
+    size_t fragSpvFileSz = std::filesystem::file_size("./shader/shader.frag.spv");
+
+    std::ifstream fragSpvFile("./shader/shader.frag.spv", std::ios_base::binary);
+
+    std::vector<char> fragSpvFileData(fragSpvFileSz);
+    fragSpvFile.read(fragSpvFileData.data(), fragSpvFileSz);
+
+    vk::ShaderModuleCreateInfo fragShaderCreateInfo;
+    fragShaderCreateInfo.codeSize = fragSpvFileSz;
+    fragShaderCreateInfo.pCode = reinterpret_cast<const uint32_t*>(fragSpvFileData.data());
+
+    vk::UniqueShaderModule fragShader = device->createShaderModuleUnique(fragShaderCreateInfo);
+
+    vk::PipelineShaderStageCreateInfo shaderStage[2];
+    shaderStage[0].stage = vk::ShaderStageFlagBits::eVertex;
+    shaderStage[0].module = vertShader.get();
+    shaderStage[0].pName = "main";
+    shaderStage[1].stage = vk::ShaderStageFlagBits::eFragment;
+    shaderStage[1].module = fragShader.get();
+    shaderStage[1].pName = "main";
+
     vk::GraphicsPipelineCreateInfo pipelineCreateInfo;
     pipelineCreateInfo.pViewportState = &viewportState;
     pipelineCreateInfo.pVertexInputState = &vertexInputInfo;
@@ -200,29 +261,131 @@ int main() {
     pipelineCreateInfo.pMultisampleState = &multisample;
     pipelineCreateInfo.pColorBlendState = &blend;
     pipelineCreateInfo.layout = pipelineLayout.get();
-    pipelineCreateInfo.stageCount = 0;
-    pipelineCreateInfo.pStages = nullptr;
     pipelineCreateInfo.renderPass = renderpass.get();
     pipelineCreateInfo.subpass = 0;
+    pipelineCreateInfo.stageCount = 2;
+    pipelineCreateInfo.pStages = shaderStage;
 
     vk::UniquePipeline pipeline = device->createGraphicsPipelineUnique(nullptr, pipelineCreateInfo).value;
 
-    vk::CommandBufferBeginInfo cmdBeginInfo;
-    cmdBufs[0]->begin(cmdBeginInfo);
+    std::vector<vk::UniqueImageView> swapchainImageViews(swapchainImages.size());
 
-    // コマンドを記録　今は空
+    for (size_t i = 0; i < swapchainImages.size(); i++) {
+        vk::ImageViewCreateInfo imgViewCreateInfo;
+        imgViewCreateInfo.image = swapchainImages[i];
+        imgViewCreateInfo.viewType = vk::ImageViewType::e2D;
+        imgViewCreateInfo.format = swapchainFormat.format;
+        imgViewCreateInfo.components.r = vk::ComponentSwizzle::eIdentity;
+        imgViewCreateInfo.components.g = vk::ComponentSwizzle::eIdentity;
+        imgViewCreateInfo.components.b = vk::ComponentSwizzle::eIdentity;
+        imgViewCreateInfo.components.a = vk::ComponentSwizzle::eIdentity;
+        imgViewCreateInfo.subresourceRange.aspectMask = vk::ImageAspectFlagBits::eColor;
+        imgViewCreateInfo.subresourceRange.baseMipLevel = 0;
+        imgViewCreateInfo.subresourceRange.levelCount = 1;
+        imgViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+        imgViewCreateInfo.subresourceRange.layerCount = 1;
 
-    cmdBufs[0]->end();
-    //コマンドバッファを投げつける用のバッファ配列に格納
-    vk::CommandBuffer submitCmdBuf[1] = { cmdBufs[0].get() };
-    //投げつける用の情報を設定
-    vk::SubmitInfo submitInfo;
-    submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = submitCmdBuf;
-    //キューに投げつける
-    graphicsQueue.submit({ submitInfo }, nullptr);
-    //キューが空になるまで待機
-    graphicsQueue.waitIdle();
+        swapchainImageViews[i] = device->createImageViewUnique(imgViewCreateInfo);
+    }
 
+    std::vector<vk::UniqueFramebuffer> swapchainFramebufs(swapchainImages.size());
+
+    for (size_t i = 0; i < swapchainImages.size(); i++) {
+        vk::ImageView frameBufAttachments[1];
+        frameBufAttachments[0] = swapchainImageViews[i].get();
+
+        vk::FramebufferCreateInfo frameBufCreateInfo;
+        frameBufCreateInfo.width = surfaceCapabilities.currentExtent.width;
+        frameBufCreateInfo.height = surfaceCapabilities.currentExtent.height;
+        frameBufCreateInfo.layers = 1;
+        frameBufCreateInfo.renderPass = renderpass.get();
+        frameBufCreateInfo.attachmentCount = 1;
+        frameBufCreateInfo.pAttachments = frameBufAttachments;
+
+        swapchainFramebufs[i] = device->createFramebufferUnique(frameBufCreateInfo);
+    }
+
+    vk::CommandPoolCreateInfo cmdPoolCreateInfo;
+    cmdPoolCreateInfo.queueFamilyIndex = graphicsQueueFamilyIndex;
+    cmdPoolCreateInfo.flags = vk::CommandPoolCreateFlagBits::eResetCommandBuffer;
+    vk::UniqueCommandPool cmdPool = device->createCommandPoolUnique(cmdPoolCreateInfo);
+
+    vk::CommandBufferAllocateInfo cmdBufAllocInfo;
+    cmdBufAllocInfo.commandPool = cmdPool.get();
+    cmdBufAllocInfo.commandBufferCount = 1;
+    cmdBufAllocInfo.level = vk::CommandBufferLevel::ePrimary;
+    std::vector<vk::UniqueCommandBuffer> cmdBufs =
+        device->allocateCommandBuffersUnique(cmdBufAllocInfo);
+
+    vk::FenceCreateInfo fenceCreateInfo;
+    vk::UniqueFence swapchainImgFence = device->createFenceUnique(fenceCreateInfo);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        device->resetFences({ swapchainImgFence.get() });
+
+        vk::ResultValue acquireImgResult = device->acquireNextImageKHR(swapchain.get(), 1'000'000'000, {}, swapchainImgFence.get());
+        if (acquireImgResult.result != vk::Result::eSuccess) {
+            std::cerr << "次フレームの取得に失敗しました。" << std::endl;
+            return -1;
+        }
+        uint32_t imgIndex = acquireImgResult.value;
+
+        if (device->waitForFences({ swapchainImgFence.get() }, VK_TRUE, 1'000'000'000) != vk::Result::eSuccess) {
+            std::cerr << "次フレームの取得に失敗しました。" << std::endl;
+            return -1;
+        }
+
+        cmdBufs[0]->reset();
+
+        vk::CommandBufferBeginInfo cmdBeginInfo;
+        cmdBufs[0]->begin(cmdBeginInfo);
+
+        vk::ClearValue clearVal[1];
+        clearVal[0].color.float32[0] = 0.0f;
+        clearVal[0].color.float32[1] = 0.0f;
+        clearVal[0].color.float32[2] = 0.0f;
+        clearVal[0].color.float32[3] = 1.0f;
+
+        vk::RenderPassBeginInfo renderpassBeginInfo;
+        renderpassBeginInfo.renderPass = renderpass.get();
+        renderpassBeginInfo.framebuffer = swapchainFramebufs[imgIndex].get();
+        renderpassBeginInfo.renderArea = vk::Rect2D({ 0,0 }, { screenWidth, screenHeight });
+        renderpassBeginInfo.clearValueCount = 1;
+        renderpassBeginInfo.pClearValues = clearVal;
+
+        cmdBufs[0]->beginRenderPass(renderpassBeginInfo, vk::SubpassContents::eInline);
+
+        cmdBufs[0]->bindPipeline(vk::PipelineBindPoint::eGraphics, pipeline.get());
+        cmdBufs[0]->draw(3, 1, 0, 0);
+
+        cmdBufs[0]->endRenderPass();
+
+        cmdBufs[0]->end();
+
+        vk::CommandBuffer submitCmdBuf[1] = { cmdBufs[0].get() };
+        vk::SubmitInfo submitInfo;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = submitCmdBuf;
+        graphicsQueue.submit({ submitInfo }, nullptr);
+
+        graphicsQueue.waitIdle();
+
+        vk::PresentInfoKHR presentInfo;
+
+        auto presentSwapchains = { swapchain.get() };
+        auto imgIndices = { imgIndex };
+
+        presentInfo.swapchainCount = presentSwapchains.size();
+        presentInfo.pSwapchains = presentSwapchains.begin();
+        presentInfo.pImageIndices = imgIndices.begin();
+
+        graphicsQueue.presentKHR(presentInfo);
+
+        graphicsQueue.waitIdle();
+    }
+
+    glfwTerminate();
     return 0;
 }
